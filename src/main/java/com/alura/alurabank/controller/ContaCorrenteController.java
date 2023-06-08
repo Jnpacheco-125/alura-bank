e52da6ce388adaf2f2886c8bc9d6bf614e3dddab
Jnpacheco-125 <RM350127@fiap.com.br>
2023-06-08 19:31:21 -0300

    Classe conta corrente, correntista e post e delete

--- src/main/java/com/alura/alurabank/controller/ContaCorrenteController.java
+++ src/main/java/com/alura/alurabank/controller/ContaCorrenteController.java
@@ -1,12 +1,15 @@
 package com.alura.alurabank.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.alura.alurabank.dominio.ContaCorrente;
+import com.alura.alurabank.dominio.Correntista;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/contas")
 public class ContaCorrenteController {
-    @GetMapping("/contas")
+    @GetMapping
     public String consultarSaldo(
             @RequestParam(name = "banco" ) String banco,
             @RequestParam(name = "agencia" )String agencia,
@@ -14,5 +17,15 @@ public class ContaCorrenteController {
         return String.format("Banco: %s, Agencia: %s, Conta: %s. Saldo: R$1300,00",
                 banco, agencia, numero);
     }
+    @PostMapping
+    public ResponseEntity<ContaCorrente> criarNovaConta(@RequestBody Correntista correntista){
+        ContaCorrente conta = new ContaCorrente("111","2222","3333");
+        return ResponseEntity.status(HttpStatus.CREATED).body(conta);
+    }
+    @DeleteMapping
+    private String fecharConta(ContaCorrente conta){
+        return "Conta fechada com sucesso";
+    }
+
 // http://localhost:8080/contas?banco=888&agencia=1111&numero=3333
 }
---
+++ src/main/java/com/alura/alurabank/dominio/Correntista.java
@@ -0,0 +1,15 @@
+package com.alura.alurabank.dominio;
+
+public class Correntista {
+
+    private String cpf;
+    private String nome;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
